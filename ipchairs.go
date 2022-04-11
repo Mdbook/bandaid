72b270b6898b3c4e611787c95a9d64d48f84a05d
MdbookTech <max@mdbook.me>
2022-04-10 18:09:11 -0700

    Patched args for ipchairs

--- ipchairs.go
+++ ipchairs.go
@@ -103,7 +103,7 @@ func (a *IpChairs) Enter() {
 			return
 		case "d", "drop-first":
 			if len(args) == 2 {
-				switch args[2] {
+				switch args[1] {
 				case "on":
 					a.config.basicFlush = true
 				case "off":
@@ -120,7 +120,7 @@ func (a *IpChairs) Enter() {
 			}
 		case "f", "flush-only":
 			if len(args) == 2 {
-				switch args[2] {
+				switch args[1] {
 				case "on":
 					a.config.onlyFlush = true
 				case "off":
@@ -137,7 +137,7 @@ func (a *IpChairs) Enter() {
 			}
 		case "i", "iron-wall":
 			if len(args) == 2 {
-				switch args[2] {
+				switch args[1] {
 				case "on":
 					a.config.safeMode = false
 				case "off":
@@ -154,7 +154,7 @@ func (a *IpChairs) Enter() {
 			}
 		case "p", "ignore-ping":
 			if len(args) == 2 {
-				switch args[2] {
+				switch args[1] {
 				case "on":
 					a.config.allowICMP = false
 				case "off":
